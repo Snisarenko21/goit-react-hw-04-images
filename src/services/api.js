fb4e0532ab94fdc72c77cd77ee332d04ded0ceb6
Snisarenko21 <sepalena@gmail.com>
2022-07-21 12:53:33 +0300

    1

--- src/services/api.js
+++ src/services/api.js
@@ -6,6 +6,7 @@ const API_KEY = '27687419-9b7122bc0ad1f15a1028fb6b8';
 function fetchPicture(name, page) {
   const response = axios.get(
     `?q=${name}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    // https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12
   );
 
   if (!response) {
